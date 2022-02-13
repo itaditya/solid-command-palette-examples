397cbfa2822244a78a4230e03d6b607a1e023519
itaditya <adityaa803@gmail.com>
2022-02-13 18:31:11 +0530

    use latest version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // render inside top level Solid component
 import { createSignal } from 'solid-js';
-import { Root, CommandPalette } from 'solid-command-palette';
+import { Root, CommandPalette, KbdShortcut } from 'solid-command-palette';
 import { actions } from './actions';
 
 import './style.css';
@@ -21,9 +21,13 @@ const App = () => {
     <div class="my-app">
       <Root actions={actions} actionsContext={actionsContext}>
         <CommandPalette />
-        <h2>Open it with CMD + K</h2>
+        <h2>
+          Open it with <KbdShortcut shortcut="$mod+k" />
+        </h2>
         <p>Current Count is {count()}</p>
-        <p>Increment Count by pressing CMD + E</p>
+        <p>
+          Increment Count by pressing <KbdShortcut shortcut="$mod+e" />
+        </p>
       </Root>
     </div>
   );
